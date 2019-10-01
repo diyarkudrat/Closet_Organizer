950144f8fe0643bb2e8352603386419fcea31896
Diyar Kudrat <dkudrat@gmail.com>
2019-10-01 14:56:43 -0700

    added MongoDB

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
-from pymongo import MongoClient
 from bson.objectid import ObjectId
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.SneakerCentral
+sneakers = db.sneakers
 
 
 app = Flask(__name__)
 
 
-sneakers = [
-    {'name': 'Air Jordan 4 Bred', 'brand': 'Air Jordan', 'price': '$190.00'},
-    {'name': 'VaporMax Plus', 'brand': 'Nike', 'price': '$180.00'}
-]
+# sneakers = [
+#     {'name': 'Air Jordan 4 Bred', 'brand': 'Air Jordan', 'price': '$190.00'},
+#     {'name': 'VaporMax Plus', 'brand': 'Nike', 'price': '$180.00'}
+# ]
 
 @app.route('/')
 def sneakers_index():
 
-    return render_template('sneakers_index.html', sneakers=sneakers)
+    return render_template('sneakers_index.html', sneakers=sneakers.find())
 
 
 if __name__ == '__main__':
