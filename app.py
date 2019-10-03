d928eb906f2842c60340626f40950f157aee6a3b
Diyar Kudrat <dkudrat@gmail.com>
2019-10-03 10:27:19 -0700

    ignore

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def sneakers_index():
 
 @app.route('/sneakers/<sneaker_id>/delete', methods = ['POST'])
 def sneakers_delete(sneaker_id):
-    sneakers.delete_one({'_id': ObjectId(playlist_id)})
+    sneakers.delete_one({'_id': ObjectId(sneaker_id)})
     return redirect(url_for('sneakers_index'))
 
 @app.route('/sneakers/<sneaker_id>', methods = ['POST'])
@@ -42,20 +42,20 @@ def sneakers_edit(sneaker_id):
     return render_template('sneakers_edit.html', sneaker = sneaker)
 
 @app.route('/sneakers/<sneaker_id>')
-def sneaker_show(sneaker_id):
+def sneakers_show(sneaker_id):
     sneaker = sneakers.find_one({'_id': ObjectId(sneaker_id)})
     return render_template('sneakers_show.html', sneaker = sneaker)
 
 @app.route('/sneakers/new')
 def sneaker_new():
-    return render_template('sneakers_new.html')
+    return render_template('sneakers_new.html', sneaker = {}, name = 'New Shoe')
 
 @app.route('/sneakers', methods=['POST'])
 def sneaker_create():
     sneaker = {
             'name': request.form.get('name'),
             'brand': request.form.get('brand'),
-            'photo': request.form.get('Photos').split()
+            # 'photo': request.form.get('Photos').split()
     }
     sneaker_id = sneakers.insert_one(sneaker).inserted_id
     return redirect(url_for('sneakers_show', sneaker_id=sneaker_id))
